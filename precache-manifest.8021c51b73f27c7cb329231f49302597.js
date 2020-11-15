fb5d72c26df34b854fd77d8ec042fa73c41543e0
Leszek Parkot <cedro1812@gmail.com>
2020-11-15 14:11:12 +0100

    Updates

---
+++ precache-manifest.8021c51b73f27c7cb329231f49302597.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c55a5e4cf4ce910551c60e07431d1",
+    "url": "/confilogi-recruitment/index.html"
+  },
+  {
+    "revision": "05e9f9ec6884c909c495",
+    "url": "/confilogi-recruitment/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "eefe61c1924f71d8b86b",
+    "url": "/confilogi-recruitment/static/css/main.7fb72de1.chunk.css"
+  },
+  {
+    "revision": "05e9f9ec6884c909c495",
+    "url": "/confilogi-recruitment/static/js/2.8860ed34.chunk.js"
+  },
+  {
+    "revision": "ac27d415db6a8f4ccefe45d91d40e399",
+    "url": "/confilogi-recruitment/static/js/2.8860ed34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefe61c1924f71d8b86b",
+    "url": "/confilogi-recruitment/static/js/main.1cd97266.chunk.js"
+  },
+  {
+    "revision": "77c35db4da8576d3cdac",
+    "url": "/confilogi-recruitment/static/js/runtime-main.324aa6ab.js"
+  },
+  {
+    "revision": "080348e5f1ac0d69c936159ae62863e1",
+    "url": "/confilogi-recruitment/static/media/icon-arrow.080348e5.svg"
+  },
+  {
+    "revision": "8132811b6f86998dea9790de1993d1de",
+    "url": "/confilogi-recruitment/static/media/icon-close.8132811b.svg"
+  },
+  {
+    "revision": "e3defca190ae3f2aaeedefe79c125bd6",
+    "url": "/confilogi-recruitment/static/media/icon-hamburger.e3defca1.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/confilogi-recruitment/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "11b0ec333ed9d17690ada04979c489c0",
+    "url": "/confilogi-recruitment/static/media/illustration-features-tab-2.11b0ec33.svg"
+  },
+  {
+    "revision": "7f696303479b76b89e45fcf33b7aff4b",
+    "url": "/confilogi-recruitment/static/media/illustration-features-tab-3.7f696303.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/confilogi-recruitment/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "53989d2a42baf707e1d92f5d05c7899b",
+    "url": "/confilogi-recruitment/static/media/logo-bookmark.53989d2a.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/confilogi-recruitment/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/confilogi-recruitment/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/confilogi-recruitment/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
